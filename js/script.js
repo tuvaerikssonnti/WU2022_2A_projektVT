aaf4eec5924696c9def424b76b74f3f2341ab704
tuvaerikssonnti <93259510+tuvaerikssonnti@users.noreply.github.com>
2022-03-25 11:06:04 +0100

    upd

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ function toggleMenu() {
 }
 
 burger.addEventListener('click', toggleMenu)
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
