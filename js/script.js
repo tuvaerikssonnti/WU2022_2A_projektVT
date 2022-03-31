bed26aabff87dc3131b948fc9b35afb2395f0285
tuvaerikssonnti <93259510+tuvaerikssonnti@users.noreply.github.com>
2022-03-31 16:41:39 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -2,20 +2,10 @@ const nav = document.getElementById('nav-links')
 const burger = document.getElementById('meny')
 
 function toggleMenu() {
-    console.log("HEJ")
+    console.log("")
     nav.classList.toggle('nav-active')
     burger.classList.toggle('crossed_line')
 }
 
 burger.addEventListener('click', toggleMenu)
 
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() { 
-  $('slideshow > div:first')
-  .fadeOut(1000)
-  .next()
-  .fadeIn(1000)
-  .end()
-  .appendTo('#slideshow');
-}, 3000);
